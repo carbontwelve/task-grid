79058eecbb732ff7cd07f60aaefdea47000d50b2
Simon Dann <simon.dann@gmail.com>
2021-02-03 22:21:31 +0000

    bugfix :: correctly set origin

--- resources/views/oauth/callback.blade.php
+++ resources/views/oauth/callback.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <title>{{ config('app.name') }}</title>
     <script>
-        window.opener.postMessage({{ json_encode($payload) }}, "{{ url('/') }}")
+        window.opener.postMessage(@json($payload), "{{ env('CLIENT_BASE_URL') }}")
         window.close()
     </script>
 </head>
